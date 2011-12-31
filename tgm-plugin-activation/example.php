87138b9f8d893a4360a678f1e859145887ffc3f9
Thomas Griffin <jthomas.griffin@gmail.com>
2011-12-31 10:24:34 -0500

    updated to reflect new 'external_url' parameter

--- tgm-plugin-activation/example.php
+++ tgm-plugin-activation/example.php
@@ -51,6 +51,7 @@ function my_theme_register_required_plugins() {
 			'version' 				=> '', // E.g. 1.0.0. If set, the active plugin must be this version or higher, otherwise a notice is presented
 			'force_activation' 		=> false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch
 			'force_deactivation' 	=> false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins
+			'external_url' 			=> '', // If set, overrides default API URL and points to an external URL
 		),
 
 		// This is an example of how to include a plugin from the WordPress Plugin Repository
