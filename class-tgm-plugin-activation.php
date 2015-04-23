de5ceef907d59ae9a56082c3a4f767db4de5b047
jrfnl <github_nospam@adviesenzo.nl>
2015-04-23 11:49:37 +0200

    Pre-empt Theme check change to disallow add_theme_page
    Ref:
    * https://make.wordpress.org/themes/2015/04/21/this-weeks-meeting-important-information-regarding-theme-options/
    * the discussion under https://github.com/thomasgriffin/TGM-Plugin-Activation/commit/394f75d3e692d8f5c29004461b0f88cf37e0a071

--- class-tgm-plugin-activation.php
+++ class-tgm-plugin-activation.php
@@ -403,7 +403,7 @@ if ( ! class_exists( 'TGM_Plugin_Activation' ) ) {
 		 */
 		protected function add_admin_menu( array $args ) {
 			if ( apply_filters( 'tgmpa_admin_menu_use_add_theme_page', true ) ) {
-				$this->page_hook = add_theme_page( $args['page_title'], $args['menu_title'], $args['capability'], $args['menu_slug'], $args['function'] );
+				$this->page_hook = call_user_func( 'add_theme_page', $args['page_title'], $args['menu_title'], $args['capability'], $args['menu_slug'], $args['function'] );
 			}
 			else {
 				$this->page_hook = call_user_func( 'add_submenu_page', $args['parent_slug'], $args['page_title'], $args['menu_title'], $args['capability'], $args['menu_slug'], $args['function'] );
