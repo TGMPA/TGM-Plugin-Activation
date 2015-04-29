0dcbc77342b9ba4506048946035bc9a1dd972d06
jrfnl <github_nospam@adviesenzo.nl>
2015-04-29 18:00:17 +0200

    Improve text string layout & ease of translatability

--- class-tgm-plugin-activation.php
+++ class-tgm-plugin-activation.php
@@ -263,22 +263,63 @@ if ( ! class_exists( 'TGM_Plugin_Activation' ) ) {
 				'menu_title'                      => __( 'Install Plugins', 'tgmpa' ),
 				'installing'                      => __( 'Installing Plugin: %s', 'tgmpa' ),
 				'oops'                            => __( 'Something went wrong.', 'tgmpa' ),
-				'notice_can_install_required'     => _n_noop( 'This theme requires the following plugin: %1$s.', 'This theme requires the following plugins: %1$s.', 'tgmpa' ),
-				'notice_can_install_recommended'  => _n_noop( 'This theme recommends the following plugin: %1$s.', 'This theme recommends the following plugins: %1$s.', 'tgmpa' ),
-				'notice_cannot_install'           => _n_noop( 'Sorry, but you do not have the correct permissions to install the %s plugin. Contact the administrator of this site for help on getting the plugin installed.', 'Sorry, but you do not have the correct permissions to install the %s plugins. Contact the administrator of this site for help on getting the plugins installed.', 'tgmpa' ),
-				'notice_can_activate_required'    => _n_noop( 'The following required plugin is currently inactive: %1$s.', 'The following required plugins are currently inactive: %1$s.', 'tgmpa' ),
-				'notice_can_activate_recommended' => _n_noop( 'The following recommended plugin is currently inactive: %1$s.', 'The following recommended plugins are currently inactive: %1$s.', 'tgmpa' ),
-				'notice_cannot_activate'          => _n_noop( 'Sorry, but you do not have the correct permissions to activate the %s plugin. Contact the administrator of this site for help on getting the plugin activated.', 'Sorry, but you do not have the correct permissions to activate the %s plugins. Contact the administrator of this site for help on getting the plugins activated.', 'tgmpa' ),
-				'notice_ask_to_update'            => _n_noop( 'The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.', 'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.', 'tgmpa' ),
-				'notice_cannot_update'            => _n_noop( 'Sorry, but you do not have the correct permissions to update the %s plugin. Contact the administrator of this site for help on getting the plugin updated.', 'Sorry, but you do not have the correct permissions to update the %s plugins. Contact the administrator of this site for help on getting the plugins updated.', 'tgmpa' ),
-				'install_link'                    => _n_noop( 'Begin installing plugin', 'Begin installing plugins', 'tgmpa' ),
-				'activate_link'                   => _n_noop( 'Begin activating plugin', 'Begin activating plugins', 'tgmpa' ),
+				'notice_can_install_required'     => _n_noop(
+					'This theme requires the following plugin: %1$s.',
+					'This theme requires the following plugins: %1$s.',
+					'tgmpa'
+				),
+				'notice_can_install_recommended'  => _n_noop(
+					'This theme recommends the following plugin: %1$s.',
+					'This theme recommends the following plugins: %1$s.',
+					'tgmpa'
+				),
+				'notice_cannot_install'           => _n_noop(
+					'Sorry, but you do not have the correct permissions to install the %s plugin.',
+					'Sorry, but you do not have the correct permissions to install the %s plugins.',
+					'tgmpa'
+				),
+				'notice_can_activate_required'    => _n_noop(
+					'The following required plugin is currently inactive: %1$s.',
+					'The following required plugins are currently inactive: %1$s.',
+					'tgmpa'
+				),
+				'notice_can_activate_recommended' => _n_noop(
+					'The following recommended plugin is currently inactive: %1$s.',
+					'The following recommended plugins are currently inactive: %1$s.',
+					'tgmpa'
+				),
+				'notice_cannot_activate'          => _n_noop(
+					'Sorry, but you do not have the correct permissions to activate the %s plugin.',
+					'Sorry, but you do not have the correct permissions to activate the %s plugins.',
+					'tgmpa'
+				),
+				'notice_ask_to_update'            => _n_noop(
+					'The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.',
+					'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.',
+					'tgmpa'
+				),
+				'notice_cannot_update'            => _n_noop(
+					'Sorry, but you do not have the correct permissions to update the %s plugin.',
+					'Sorry, but you do not have the correct permissions to update the %s plugins.',
+					'tgmpa'
+				),
+				'install_link'                    => _n_noop(
+					'Begin installing plugin',
+					'Begin installing plugins',
+					'tgmpa'
+				),
+				'activate_link'                   => _n_noop(
+					'Begin activating plugin',
+					'Begin activating plugins',
+					'tgmpa'
+				),
 				'return'                          => __( 'Return to Required Plugins Installer', 'tgmpa' ),
 				'dashboard'                       => __( 'Return to the dashboard', 'tgmpa' ),
 				'plugin_activated'                => __( 'Plugin activated successfully.', 'tgmpa' ),
 				'activated_successfully'          => __( 'The following plugin was activated successfully:', 'tgmpa' ),
 				'complete'                        => __( 'All plugins installed and activated successfully. %1$s', 'tgmpa' ),
 				'dismiss'                         => __( 'Dismiss this notice', 'tgmpa' ),
+				'contact_admin'                   => __( 'Please contact the administrator of this site for help.', 'tgmpa' ),
 			);
 
 			do_action( 'tgmpa_register' );
@@ -853,6 +894,10 @@ if ( ! class_exists( 'TGM_Plugin_Activation' ) ) {
 							$count
 						)
 					);
+
+					if ( 0 === strpos( $type, 'notice_cannot' ) ) {
+						$rendered .= $this->strings['contact_admin'];
+					}
 				}
 				unset( $type, $plugin_group, $linked_plugins, $count, $last_plugin, $imploded );
 
