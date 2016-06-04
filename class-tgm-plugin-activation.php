ff0f5e827acb25c173860bf8d8aab28f3f6581d9
Ninos Ego <me@ninosego.de>
2016-06-04 02:40:59 +0200

    CS

--- class-tgm-plugin-activation.php
+++ class-tgm-plugin-activation.php
@@ -32,7 +32,7 @@
 	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-// Exit if accessed directly
+// Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
