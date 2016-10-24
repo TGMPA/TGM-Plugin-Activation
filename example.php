f5a3b2e741d87dba7415b1003470fcb13107155d
Mark McEver <mark.mcever@vanderbilt.edu>
2016-10-24 10:27:08 -0500

    Fixed an invalid path preventing the install of the bundled example plugin

--- example.php
+++ example.php
@@ -63,7 +63,7 @@ function my_theme_register_required_plugins() {
 		array(
 			'name'               => 'TGM Example Plugin', // The plugin name.
 			'slug'               => 'tgm-example-plugin', // The plugin slug (typically the folder name).
-			'source'             => get_stylesheet_directory() . '/lib/plugins/tgm-example-plugin.zip', // The plugin source.
+			'source'             => get_stylesheet_directory() . '/plugins/tgm-example-plugin.zip', // The plugin source.
 			'required'           => true, // If false, the plugin is only 'recommended' instead of required.
 			'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
 			'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
