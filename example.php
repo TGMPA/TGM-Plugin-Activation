18d3db735988ae2a449f98be43dfbb43fedabc94
Adrien Foulon <6115458+Tofandel@users.noreply.github.com>
2018-08-01 11:31:55 +0200

    Updating the example with class_exists

--- example.php
+++ example.php
@@ -70,6 +70,7 @@ function my_theme_register_required_plugins() {
 			'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
 			'external_url'       => '', // If set, overrides default API URL and points to an external URL.
 			'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
+			'class_exists'	     => '' // Same behavior as is_callable, this checks if a class exists to determine if a plugin is active
 		),
 
 		// This is an example of how to include a plugin from an arbitrary external source in your theme.
